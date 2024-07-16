ba91cb070fcb38c56b4dfb8d843639adc1e2929c
AlexBirch123 <167043639+AlexBirch123@users.noreply.github.com>
2024-07-16 19:58:21 -0300

    Add files via upload

---
+++ CRUD_Sucursal.2.js
@@ -0,0 +1,158 @@
+//Eventos de los botones por ID
+const b_new = document.getElementById('b_new');
+const b_show = document.getElementById('b_show');
+const b_mod = document.getElementById('b_mod');
+const b_del = document.getElementById('b_del');
+const form = document.getElementById('sucData');
+const dir = document.getElementById('dir');
+const tel = document.getElementById('tele');
+const enviar = document.getElementById('b_carga');
+const listSuc = document.getElementById('muestraSuc');
+const cerrarListaSuc = document.getElementById('cerrarLista');
+
+b_new.addEventListener('click', () => {
+  showForm();
+  console.log('nueva sucursal');
+});
+
+b_show.addEventListener('click', () => {
+  showDivSucursales();
+  p.showSucursal();
+  // console.log('mostrar sucursal numero:');
+});
+
+b_mod.addEventListener('click', () => {
+  let op = Number(prompt('Ingrese ID de la sucursal'));
+  p.updateSucursal(op);
+  console.log('Se modifico una sucursal');
+});
+
+b_del.addEventListener('click', () => {
+  let op = Number(prompt('Ingrese ID de la sucursal'));
+  p.delSucursal(op);
+  console.log('eliminada');
+});
+
+enviar.addEventListener('click', () => {
+  let direc = dir.value;
+  let ntel = tel.value;
+  p.creatSucursal(direc, ntel);
+});
+
+cerrarListaSuc.addEventListener('click', () => {
+  showDivSucursales();
+});
+
+
+//Creacion de Clases y metodos
+
+class Pintureria {
+  sucursales = [];
+  nombre;
+  constructor(nombre) {
+    this.nombre = nombre;
+  }
+
+  creatSucursal(direc, ntel) {
+    let id = this.sucursales.length + 1;
+    const s = new Sucursal(id, direc, ntel);
+    console.log(s);
+    this.sucursales.push(s); // agrega nueva sucursal al array sucursales
+  }
+
+  // Muestra las sucursales cargadas
+  showSucursal() {
+    this.sucursales.forEach((sucursal) => {
+      const li = document.createElement("li")
+      const d = document.createElement("a")
+      const t = document.createElement("a")
+      li.innerHTML = '<strong>ID: </strong>'+ sucursal.id;
+      d.innerHTML = '<strong>Dirección: </strong>'+ sucursal.direccion;
+      t.innerHTML = '<br><strong>Telefono: </strong>'+ sucursal.telefono;
+      listSuc.appendChild(li);
+      listSuc.appendChild(d);
+      listSuc.appendChild(t);
+    });
+  }
+
+  //Busca una sucursal por id
+  searchSucursal(idsuc) {
+    const f = this.sucursales.find((sucursal) => sucursal.id === idsuc);
+    console.log(f);
+    return f;
+  }
+
+  //Elimina una sucursal
+  delSucursal(id) {
+    const suc = this.sucursales.findIndex((sucursal) => sucursal.id === id);
+    if (suc !== -1) {
+      this.sucursales.splice(suc, 1);
+      return console.log('Sucursal eliminada con exito');
+    } else console.error('sucursal no encontrada');
+    return 0;
+  }
+
+  //Actualiza una sucursal
+  updateSucursal(id) {
+    const suc = this.searchSucursal(id);
+    console.log(suc);
+    if (suc !== undefined) {
+      suc.direccion = prompt('Ingrese direccion de la sucursal');
+      suc.telefono = prompt('Ingrese telefono de la sucursal');
+      console.log(suc);
+      return 0;
+    } else console.error('sucursal no encontrada');
+    return 0;
+  }
+}
+
+class Sucursal {
+  id;
+  direccion;
+  telefono;
+  productos = [];
+  personas = [];
+  ventas = [];
+  constructor(id, direccion, telefono) {
+    this.id = id;
+    this.direccion = direccion;
+    this.telefono = telefono;
+  }
+}
+
+//Muestra y oculta el formulario de sucursales
+function showForm() {
+  if (form.style.display == 'block') {
+    form.style.display = 'none';
+    b_show.style.display = 'block';
+    b_mod.style.display = 'block';
+    b_del.style.display = 'block';
+    b_new.innerHTML = '<strong>Nueva sucursal</strong>';
+  } else {
+    form.style.display = 'block';
+    b_show.style.display = 'none';
+    b_mod.style.display = 'none';
+    b_del.style.display = 'none';
+    b_new.innerHTML = '<strong style = "color:red">Cancelar</strong>';
+  }
+}
+
+function showDivSucursales() {
+  if (listSuc.style.display == 'block') {
+    listSuc.style.display = 'none';
+    b_show.style.display = 'block';
+    b_new.style.display = 'block';
+    b_mod.style.display = 'block';
+    b_del.style.display = 'block';
+    cerrarListaSuc.style.display = 'none';
+  } 
+  else {
+    listSuc.style.display = 'block';
+    b_show.style.display = 'none';
+    b_new.style.display = 'none';
+    b_mod.style.display = 'none';
+    b_del.style.display = 'none';
+    cerrarListaSuc.style.display = 'block';
+  }
+}
+const p = new Pintureria('Rio Color');
