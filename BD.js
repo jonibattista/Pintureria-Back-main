8351c7cd8178709431c87ff70f8849a9240b0454
Alex <Alexbirchmeyer@gmail.com>
2024-12-28 12:30:48 -0300

    Actualizacion DB

--- BD.js
+++ BD.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize("pintureria", "root", "root", {
+export const sequelize = new Sequelize("pintureria", "root", "123456789", {
   host: "localhost",
   dialect: "mysql",
 });
