130d95714e4d74edd5386e868701bdc0eb89610f
Alex <Alexbirchmeyer@gmail.com>
2025-01-16 17:55:32 -0300

    Actualiza las rutas de sucursales para usar PATCH

--- Sucursales/Sucursales.routes.js
+++ Sucursales/Sucursales.routes.js
@@ -1,10 +1,10 @@
-import { Router } from 'express';
-import { add, getAll, getOne, remove, update } from './Sucursal.controllers.js';
+import { Router } from "express";
+import { add, getAll, getOne, remove, update } from "./Sucursal.controllers.js";
 
 export const routerSuc = Router();
 
-routerSuc.get('/', getAll);
-routerSuc.get('/:id', getOne);
-routerSuc.post('/', add);
-routerSuc.put('/:id', update);
-routerSuc.delete('/:id', remove);
+routerSuc.get("/", getAll);
+routerSuc.get("/:id", getOne);
+routerSuc.post("/", add);
+routerSuc.patch("/:id", update);
+routerSuc.delete("/:id", remove);
