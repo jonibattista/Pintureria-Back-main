ee2c52180ae3ae8d8eafae30b256703c7156453a
Alex <alexbichmeyer@gmail.com>
2025-01-21 13:33:24 -0300

    Actualiza las rutas de clientes

--- BD.js
+++ BD.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize("pintureria", "root", "123456789", {
+export const sequelize = new Sequelize("pintureria", "root", "root", {
   host: "localhost",
   dialect: "mysql",
 });
