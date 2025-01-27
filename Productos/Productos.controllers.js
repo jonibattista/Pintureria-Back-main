12d79db542b1f9891b880477124e7531cb3dc348
Alex <Alexbirchmeyer@gmail.com>
2025-01-27 12:35:26 -0300

    Mejora el manejo de errores productos y usuarios

--- Productos/Productos.controllers.js
+++ Productos/Productos.controllers.js
@@ -2,58 +2,73 @@ import { Product } from "./Productos.class.js";
 
 
 export const getAll = async (req, res) => {
-    Product.sync()
-    try {
-        const result = await Product.findAll();
-        res.status(200).json(result);
-    } catch (error) {
-        res.status(500).send(error);
-    }
+  Product.sync();
+  try {
+    const result = await Product.findAll();
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ message: "error al obtener los productos" });
+  }
 };
 
 export const getOne = async (req, res) => {
-    Product.sync()
-    const id = req.params.id;
-    try {
-        const result = await Product.findOne({ where: { id: id } });
-        res.status(200).json(result);
-    } catch (error) {
-        res.status(500);
-    }
+  Product.sync();
+  const id = req.params.id;
+  try {
+    const result = await Product.findOne({ where: { id: id } });
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ message: "error al obtener el producto" });
+  }
 };
 
 export const add = async (req, res) => {
-    Product.sync()
-    const { description, price, stock, idProv, idCat } = req.body;
-    try {
-        const result = await Product.create({ description: description, price: price, stock: stock, idProv: idProv, idCat: idCat });
-        res.status(201).json(result);
-    } catch (error) {
-        res.status(500).send(error);
-    }
+  Product.sync();
+  const { description, price, stock, idProv, idCat } = req.body;
+  try {
+    const result = await Product.create({
+      description: description,
+      price: price,
+      stock: stock,
+      idProv: idProv,
+      idCat: idCat,
+    });
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).json({ message: "error al agregar los productos" });
+  }
 };
 
 export const update = async (req, res) => {
-    Product.sync()
-    const { id } = req.params;
-    const { description, price, stock, idProv, idCat} = req.body;
-    try {
-        const result = await Product.update({ description: description, price: price, stock: stock, idProv: idProv ,idCat: idCat}, { where: { id: id } });
-        res.status(200).json(result);
-    } catch (error) {
-        res.status(500).send(error);
-    }
+  Product.sync();
+  const { id } = req.params;
+  const { description, price, stock, idProv, idCat } = req.body;
+  try {
+    const result = await Product.update(
+      {
+        description: description,
+        price: price,
+        stock: stock,
+        idProv: idProv,
+        idCat: idCat,
+      },
+      { where: { id: id } }
+    );
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ message: "error al actualizar los productos" });
+  }
 };
 
 export const remove = async (req, res) => {
-    Product.sync()
-    const { id } = req.params;
-    try {
-        const result = await Product.destroy({ where: { id: id } });
-        res
-            .status(200)
-            .send({ message: `producto número ${id} eliminada con exito`, result });
-    } catch (error) {
-        res.status(500);
-    }
+  Product.sync();
+  const { id } = req.params;
+  try {
+    const result = await Product.destroy({ where: { id: id } });
+    res
+      .status(200)
+      .send({ message: `producto número ${id} eliminada con exito`, result });
+  } catch (error) {
+    res.json({ message: "error al eliminar los productos" });
+  }
 };
--- Usuarios/Usuario.controllers.js
+++ Usuarios/Usuario.controllers.js
@@ -15,47 +15,46 @@ export const getAll = async (req, res) => {
 };
 
 export const getByLevel = async (req, res) => {
-
-
-  const level = req.params.level;
+  const role = req.params.role;
   try {
-    const result = await User.findAll({ where: { level: level } });
+    const result = await User.findAll({ where: { role: role } });
     res.status(200).json(result);
   } catch (error) {
-    res.status(500).json({message: 'Error al obtener los usuarios'});
+    res.status(500).json({ message: "Error al obtener los usuarios" });
   }
 };
 
 export const getByUserName = async (req, res) => {
-
   const { userName } = req.params;
-  console.log(userName)
+  console.log(userName);
   try {
     const result = await User.findOne({ where: { userName: userName } });
     res.status(200).json(result);
   } catch (error) {
-    res.status(500).json({message: 'Error al obtener el usuario'});
+    res.status(500).json({ message: "Error al obtener el usuario" });
   }
 };
 
 export const getByUserEmail = async (req, res) => {
-
   const { email } = req.params;
   try {
     const result = await User.findOne({ where: { email: email } });
     res.status(200).json(result);
   } catch (error) {
-    res.status(500).json({message: 'Error al obtener el usuario'});
+    res.status(500).json({ message: "Error al obtener el usuario" });
   }
 };
 
 export const login = async (req, res) => {
-
   const { userName, pswHash } = req.body;
   try {
     const user = await User.findOne({ where: { userName: userName } });
-    const token = jwt.sign({ id: user.id, username: user.userName, level: user.level }, SECRET_JWT, { expiresIn: "1h" })
-    
+    const token = jwt.sign(
+      { id: user.id, username: user.userName, role: user.role },
+      SECRET_JWT,
+      { expiresIn: "1h" }
+    );
+
     if (!user) {
       return res.status(400).json(user);
     }
@@ -63,61 +62,71 @@ export const login = async (req, res) => {
     if (!esCorrecta) {
       return res.status(401).send(undefined);
     }
-    return res.status(200)
-       .cookie("access_token", token, {
-         httpOnly: true,
-         maxAge: 1000 * 60 * 60,
-         secure: false, 
-         sameSite: 'lax', 
-       }).json(user.level)
-      //  .send({token:token});
+    return res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60,
+        secure: false,
+        sameSite: "lax",
+      })
+      .json(user.role);
+    //  .send({token:token});
   } catch (error) {
-    res.status(401).json({message: 'Error en el inicio de sesión'});
+    res.status(401).json({ message: "Error en el inicio de sesión" });
   }
 };
 
-export const logout =  (req, res) => {
+export const logout = (req, res) => {
   try {
-    res.clearCookie('access_token');
-    res.status(200).json({ message: 'Logout exitoso' });    
+    res.clearCookie("access_token");
+    res.status(200).json({ message: "Logout exitoso" });
   } catch (error) {
-    res.status(500).json({message: "Error al cerrar sesion"})
+    res.status(500).json({ message: "Error al cerrar sesion" });
   }
-  
 };
 
 export const register = async (req, res) => {
-
-  const { userName, email, pswHash, level } = req.body;
-  console.log(pswHash)
+  const { userName, email, pswHash, role } = req.body;
+  console.log(pswHash);
   const userExist = await User.findOne({ where: { userName: userName } });
-  if (userExist) return res.status(400).json({message: 'Nombre de usuario existente'});
+  if (userExist)
+    return res.status(400).json({ message: "Nombre de usuario existente" });
   const emialExist = await User.findOne({ where: { email: email } });
-  if (emialExist) return res.status(400).json({message: 'email existente'});
+  if (emialExist) return res.status(400).json({ message: "email existente" });
   const hash = await bcrypt.hash(pswHash, 10);
   try {
-    const result = await User.create({ userName: userName, email: email, pswHash: hash, level: level });
+    const result = await User.create({
+      userName: userName,
+      email: email,
+      pswHash: hash,
+      role: role,
+    });
     res.status(201).json(result);
   } catch (error) {
-    res.status(500).json({message: 'Error al ingresar el usuario'});
+    res.status(500).json({ message: "Error al ingresar el usuario" });
   }
 };
 
 export const update = async (req, res) => {
-
   const { id } = req.params;
-  const { userName, pswHash, level } = req.body;
+  const { userName, pswHash, role } = req.body;
   if (dni) {
-      const existingUser = await User.findOne({ where: { userName: userName } });
-      if (existingUser && existingUser.id !== id) {
-        return res.status(400).json({ message: "El nombre de usuario ya existe." });
-      }
+    const existingUser = await User.findOne({ where: { userName: userName } });
+    if (existingUser && existingUser.id !== id) {
+      return res
+        .status(400)
+        .json({ message: "El nombre de usuario ya existe." });
     }
+  }
   try {
-    const result = await User.update({ userName: userName, pswHash: pswHash, level: level }, { where: { id: id } });
+    const result = await User.update(
+      { userName: userName, pswHash: pswHash, role: role },
+      { where: { id: id } }
+    );
     res.status(200).json(result);
   } catch (error) {
-    res.status(500).json({message: 'Error al actualizar el usuario'});
+    res.status(500).json({ message: "Error al actualizar el usuario" });
   }
 };
 
