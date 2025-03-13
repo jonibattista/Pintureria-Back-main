dc24f6d1dd5e0745e9e65a7ecd4871f66633caf7
Alex <Alexbirchmeyer@gmail.com>
2025-03-13 12:47:53 -0300

    Mejorar la función de carga de imágenes

--- Imagenes/images.controllers.js
+++ Imagenes/images.controllers.js
@@ -1,17 +1,26 @@
 import multer from "multer";
-import path from "path";
 
-
+//Crea funcion storage para definir las configuraciones de multer
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, "uploads/"); // Carpeta donde se guardarán las imágenes
     },
     filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname)); // Renombra el archivo con la fecha actual
-    },
+        cb(null, Date.now() + "_" + file.originalname); 
+    }, // Renombra el archivo con la fecha actual y el nombre original
 });
 
-export const upload = multer({ storage }); //crea una instancia de multer pasandole la configuarion anterior
+const fileFilter = (req, file, cb) => {
+    // Acepta solo archivos de imagen
+    if (file.mimetype.startsWith("image/")) {
+        cb(null, true);
+    } else {
+        cb(new Error("Tipo de archivo no permitido"), false);
+    }
+};
+
+//crea una instancia de multer pasandole la configuarion anterior
+export const upload = multer({ storage }); 
 
 export const uploadImg = (req, res) => {
     try {
@@ -20,8 +29,6 @@ export const uploadImg = (req, res) => {
       }
       res.status(200).json({ message: "Imagen subida con éxito", file: req.file.filename });
     } catch (error) {
-        return res.status(500).json({ message: "No se subió ninguna imagen", error });
-    }
-    
-    
+        return res.status(500).json({ message: "Error al subir imagen" });
+    } 
 }
